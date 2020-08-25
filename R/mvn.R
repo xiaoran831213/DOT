cbe709f46b54e1d8b8ea4a17d125ef2f1542d8c7
xtong <tongxia1@msu.edu>
2020-08-25 06:47:14 -0400

    remove unnecessary multivariate normal CDF functions; drop 'within' syntex; improve gentype imputation.

--- R/mvn.R
+++ R/mvn.R
@@ -31,7 +31,7 @@ pmvn <- function(x, b=NULL, m=NULL, v=NULL, pts=1e5, tol=NULL)
         d[d < d[1] * tol] <- 0
         u %*% (sqrt(d) * t(v))
     })
-    y <- z %*% matrix(rnorm(p * pts), p, pts) + m
+    y <- z %*% matrix(stats::rnorm(p * pts), p, pts) + m
     
     r <- double(n)
     for(i in seq(n))
@@ -39,122 +39,13 @@ pmvn <- function(x, b=NULL, m=NULL, v=NULL, pts=1e5, tol=NULL)
     r
 }
 
-mpdf <- function(x, m=NULL, v=NULL)
+ts1 <- function()
 {
-    if(is.vector(x)) # points
-        x <- t(x)
-    n <- nrow(x)
-    p <- ncol(x)
-
-    if(is.null(m)) # mean
-        m <- 0
-    m <- rep(m, len=p)
-    x <- sweep(x, 2, m)
-    
-    if(is.null(v)) # covariance
-        v <- 1
-    if(is.vector(v))
-        v <- diag(v, p)
-
-    u <- chol(v)
-    a <- chol2inv(u)
-    r <- -.5 * rowSums(x %*% a * x) - sum(log(diag(u))) - .5 * log(2*pi)
-    exp(r)
-}
-
-wmvn <- function(x, b=NULL, m=NULL, v=NULL, pts=1e3)
-{
-    if(is.vector(x))                    # quantile
-        x <- t(x)
-    n <- nrow(x)
-    p <- ncol(x)
-
-    a <- 0 * x
-    b <- x
-    d <- (b - a) / pts
-
-    if(is.null(m))                      # mean
-        m <- 0
-    m <- rep(m, p)
-
-    if(is.null(v))                      # covariance
-        v <- 1
-    if(is.vector(v))
-        v <- diag(v, p)
-
-    psum <- 0 * d[, 1]
-    for(i in seq(0, pts))
-    {
-        x <- a + i * d
-        f <- mpdf(x, m, v)
-        psum <- psum + f
-    }
-    psum / pts
-}
-
-mcdf <- function(x, mu=NULL, sigma=diag(ncol(x)), a=NULL)
-{
-    if(is.vector(x))
-        x <- t(x)
-    ## Monte-Carlo confidence factor for the standard error: 99 %
-    gamma = 2.5;
-    ## Tolerance
-    err_eps = 1e-3;
-
-    ## Dimension
-    l <- nrow(x)
-    q <- nrow(sigma)
-    cases <- nrow(x)
-
-    ## Default value for mu
-    if (is.null(mu))
-        mu <- 0
-    mu <- rep(mu, len=q)
-    
-    ## move x to center
-    ## x <- scale(x, mu, FALSE)
-    x <- sweep(x, 2, mu)
-
-    ## lower bound
-    if(is.null(a))
-        a <- matrix(-Inf, cases, q)
-
-    ## Cholesky lower Tri
-    u <- t(chol(sigma))
-    
-    ## Number of integral transformations
-    n <- 1
-
-    p <- rep(0, l)
-    varsum <- rep(0, l)
-
-    err <- rep(err_eps, l)
-    
-    ## Apply crude Monte-Carlo estimation
-    while(any(err >= err_eps))
-    {
-        ## Sample from q-1 dimensional unit hypercube
-        w <- matrix(runif(cases * (q - 1)), cases, q - 1)
-        
-        ## Transformation of the multivariate normal integral
-        d <- pnorm(a[, 1] / u[1, 1])
-        e <- pnorm(x[, 1] / u[1, 1])
-        f <- e - d
-        y <- matrix(0, cases, q - 1)
-
-        for(i in seq(q - 1))
-        {
-            y[, i] <- qnorm(d + w[, i] * (e - d))
-            d <- pnorm((a[, i+1] - sum(u[i+1, 1:i] * y[, 1:i])) / u[i+1, i+1])
-            e <- pnorm((x[, i+1] - sum(u[i+1, 1:i] * y[, 1:i])) / u[i+1, i+1])
-            f <- (e - d) * f;
-        }
-        n <- n + 1
-
-        ## Estimate standard error
-        varsum <- varsum + (n - 1) * ((f - p)^2) / n
-        err <- gamma * sqrt (varsum / (n * (n - 1)))
-        p <- p + (f - p) / n
-    }
-    list(p=p, err=err)
+    x <- c(3, 3, 3)
+    b <- -x
+    m <- c(0, 0, 0)
+    v <- matrix(c(1.0, 0.5, 0.0,
+                  0.5, 1.0, 0.5,
+                  0.0, 0.5, 1.0), 3, 3)
+    pmvn(x, b, m=m, v=v, pts=1e5)
 }
--- R/sum.R
+++ R/sum.R
@@ -1,4 +1,4 @@
-#' De-correlation followed by Summarized Statistical Test
+#' Decorrelation followed by Summarized Statistical Test
 #'
 #' Summarize a  series of genetic  association test statistics  (i.e., Z-scores,
 #' \strong{Z}) given their correlation (i.e., LD-values, \strong{C}).
@@ -13,25 +13,29 @@
 #' essentially tests  the joint null: none  of the variants are  associated with
 #' the phenotype of interests.
 #'
-#' The two rank trancated tools \code{\link{dot_art}} and \code{\link{dot_rtp}},
-#' require an addnitional  parameter \code{k} specifying the  number of smallest
-#' (de-correlated) p-values  to combine,  which by default  retains half  of the
-#' p-values.
+#' The  two  rank  truncated test  \code{\link{dot_art}},  \code{\link{dot_rtp}}
+#' require an additional  parameter \code{k} specifying the  number of smallest
+#' (decorrelated) p-values  to combine,  which by default  retains half  of the
+#' p-values.  The adaptive \code{\link{dot_arta}} on the other hand, decides the
+#' appropriate \code{k} automatically.
 #'
-#' The property  of combined statistics via  \link{dot_art}, \link{dot_rtp}, and
-#' \link{dot_fisher} are detailed in the reference below.
+#' The  property  of  combined statistics  via  \link{dot_art},  \link{dot_rtp},
+#' \link{dot_arta} and \link{dot_fisher} are detailed in the reference below.
 #'
 #' @param Z vector of association test statistics (i.e., Z-scores).
 #' @param  C matrix of  correlation among the  test statistics, as  obtained by
 #'     \code{\link{cst}}.
-#' @param k controls how many top statistics (i.e., \code{k} smallist p-values)
-#'     to be combined.
+#' @param  k  controls  the  number of  statistics  (i.e.,  \code{k}  smallest,
+#'     decorrelated p-values) to combine.
 #' @param ... additional parameters
 #'
-#' @return
-#' A list  with decorrelated  association statistics \strong{X},  the orthogonal
-#' transformation \strong{W} such that  \strong{X} = \strong{WZ}, the summarized
-#' statistics Y, and the corresponding p-value P.
+#' @return typically a list, with
+#' \itemize{
+#' \item{\code{X}:} {decorrelated  association statistics}
+#' \item{\code{W}:} {orthogonal transformation, such that \code{X == WZ}}
+#' \item{\code{Y}:} {the summarized statistics}
+#' \item{\code{P}:} {the p-value corresponding to \code{Y}}
+#' }
 #'
 #' @references
 #' \href{https://www.frontiersin.org/articles/10.3389/fgene.2019.01051}{
@@ -40,8 +44,8 @@
 #' Frontiers in genetics, 10, 1051.}
 #'
 #' @name dot_sst
-#' @export
-#'
+#' @seealso \code{\link{dot}}
+#' 
 #' @examples
 #' ## get the test statistics and pre-calculated LD matrix
 #' stt <- readRDS(system.file("extdata", 'art_zsc.rds', package="dotgen"))
@@ -49,78 +53,84 @@
 NULL
 
 
-#' @describeIn dot_sst De-correlation followed by Chi-square Test
+#' @describeIn dot_sst Decorrelation followed by Chi-square Test
 #'
 #' @examples
 #'
-#' ## de-correlated chi-square test
+#' ## decorrelated chi-square test
 #' rpt <- dot_chisq(stt, sgm)
 #' print(rpt$Y)  # 37.2854
 #' print(rpt$P)  #  0.0003736988
+#' @export
 dot_chisq <- function(Z, C, ...)
 {
-    within(dot(Z, C, ...),
-    {
-        Y <- sum(X^2)
-        P <- 1 - pchisq(Y, df=length(Z))
-    })
+    ret <- dot(Z, C, ...)
+    Y <- sum(ret$X^2)
+    P <- 1 - stats::pchisq(Y, df=length(Z))
+    c(list(P=P, Y=Y), ret)
 }
 
 #' @describeIn dot_sst
 #'
-#' De-correlation followed by Augmented Rank Trucated (ART) Test 
+#' Decorrelation followed by Augmented Rank Trucated (ART) Test 
 #'
 #' @examples
 #'
-#' ## de-correlated augmented rank trucated (ART) test
-#' rpt <- dot_art(stt, sgm)
+#' ## decorrelated augmented rank trucated (ART) test
+#' rpt <- dot_art(stt, sgm, k=6)
 #' print(rpt$Y)  # 22.50976
 #' print(rpt$P)  #  0.0006704994
+#' @export
 dot_art <- function(Z, C, k, ...)
 {
     L <- length(Z)                      # number of statistics
     if(missing(k))                      # k = L / 2 (default)
         k <- round(L * .5)
 
-    within(dot(Z, C, ...),
-    {
-        ## de-correlated two-tail p-values, sorted
-        P <- sort(1 - pchisq(X^2, df=1))
+    ret <- dot(Z, C, ...)
+
+    ## decorrelated two-tail p-values, sorted
+    P <- sort(1 - stats::pchisq(ret$X^2, df=1))
         
-        ## summarized statistics: shape parameter
-        S <- (k - 1) * (digamma(L + 1) - digamma(k))
+    ## summarized statistics: shape parameter
+    S <- (k - 1) * (digamma(L + 1) - digamma(k))
 
-        ## summarized statistics: summation
-        Y <- 0
-        Y <- Y + (k - 1) * log(P[k])
-        Y <- Y - sum(log(P[1:(k - 1)]))
-        Y <- Y + qgamma(1 - pbeta(P[k], k, L - k + 1), S)
+    ## summarized statistics: summation
+    Y <- 0
+    Y <- Y + (k - 1) * log(P[k])
+    Y <- Y - sum(log(P[1:(k - 1)]))
+    Y <- Y + stats::qgamma(1 - stats::pbeta(P[k], k, L - k + 1), S)
 
-        ## summarized statistics: p-value
-        P <- 1 - pgamma(Y, k + S - 1)
-    })
+    ## summarized statistics: p-value
+    P <- 1 - stats::pgamma(Y, k + S - 1)
+
+    c(list(P=P, Y=Y), ret)
 }
 
 
 #' @describeIn dot_sst
 #'
-#' De-correlation followed by Augmented Rank Trucated Adaptive (ARTA) Test
+#' Decorrelation followed by Augmented Rank Truncated Adaptive (ARTA) Test
 #'
-#' The paramter  \code{k} limite the number  of top statistics to  combine. The
-#' actual number of statistics included is decided adaptively.
+#' \code{k} limit number of decorrelated p-values to combine; the actual number
+#' is decided adaptively.
 #'
 #' @return
-#' the actual number (\code{k}) of statistics combined
+#' For Augmented Rank Truncated Adaptive (ARTA) Test,
+#' \itemize{
+#' \item{k:} {gives the number of smallest, decorrelated p-values combined}
+#' }
 #'
 #' @param pts number of samples for MVN integration
 #' 
 #' @examples
 #'
-#' ## de-correlated augmented rank trucated (ART) test
-#' rpt <- dot_arta(stt, sgm, round(length(stt)/2), pts=1e7)
+#' ## decorrelated augmented rank truncated adaptive (ARTA) test
+#' rpt <- dot_arta(stt, sgm, k=6, pts=1e7)
 #' print(rpt$Y)  # -1.738662
 #' print(rpt$k)  #  5
 #' print(rpt$P)  #  0.003174 (varies)
+#' @export
 dot_arta <- function(Z, C, k, pts=1e6, ...)
 {
     L <- length(Z) # number of statistics
@@ -130,16 +140,16 @@ dot_arta <- function(Z, C, k, pts=1e6, ...)
     wgt <- rep(1, k)
     ret <- dot(Z, C)
 
-    P <- sort(1 - pchisq(ret$X^2, df=1))
+    P <- sort(1 - stats::pchisq(ret$X^2, df=1))
 
     . <- ((1 - P) / c(1, 1 - P[-L]))^(L:1)
     p <- (1 - .)[1:k]
 
-    sumQ <- cumsum(qnorm(p) * wgt)
+    sumQ <- cumsum(stats::qnorm(p) * wgt)
 
     pSg <- matrix(cumsum(wgt[1:k]^2), k, k)
     pSg[lower.tri(pSg)] <- t(pSg)[lower.tri(pSg)]
-    pCr <- cov2cor(pSg)
+    pCr <- stats::cov2cor(pSg)
 
     sQ <- sumQ / sqrt(diag(pSg))
 
@@ -152,56 +162,54 @@ dot_arta <- function(Z, C, k, pts=1e6, ...)
 
 #' @describeIn dot_sst
 #'
-#' De-correlation followed  by Rank Trucated Product (RTP) Test
+#' Decorrelation followed  by Rank Truncated Product (RTP) Test
 #'
 #' @examples
 #'
-#' ## de-correlated Rank Trucated Product (RTP)
-#' rpt <- dot_rtp(stt, sgm)
+#' ## decorrelated Rank Truncated Product (RTP)
+#' rpt <- dot_rtp(stt, sgm, k=6)
 #' print(rpt$Y)  # 22.6757
 #' print(rpt$P)  #  0.0007275518
+#' @export
 dot_rtp <- function(Z, C, k, ...)
 {
     L <- length(Z)                      # number of statistics
     if(missing(k))                      # k = L / 2 (default)
         k <- round(L * .5)
 
-    within(dot(Z, C, ...),
+    ret <- dot(Z, C, ...)
+
+    ## decorrelated two-tail p-values, sorted
+    P <- sort(1 - stats::pchisq(ret$X^2, df=1))
+    Y <- sum(-log(P[1:k]))
+    P <- stats::integrate(function(x, y, m, n)
     {
-        ## de-correlated two-tail p-values, sorted
-        P <- sort(1 - pchisq(X^2, df=1))
-
-        Y <- sum(-log(P[1:k]))
-
-        P <- integrate(function(x, y, m, n)
-        {
-            1-pgamma(log(qbeta(x, m + 1, n - m)) * m + y, m)
-        },
-        0, 1, Y, k, L)$va
-    })
+        1 - stats::pgamma(log(stats::qbeta(x, m + 1, n - m)) * m + y, m)
+    },
+    0, 1, Y, k, L)$va
+    c(list(P=P, Y=Y), ret)
 }
 
 
 #' @describeIn dot_sst
 #'
-#' De-correlation followed by Fisher's Combined p-value Test
+#' Decorrelation followed by Fisher's Combined p-value Test
 #'
 #' @examples
 #'
-#' ## de-correlated Fisher's combined p-value chi-square test
+#' ## decorrelated Fisher's combined p-value chi-square test
 #' rpt <- dot_fisher(stt, sgm)
 #' print(rpt$Y)  # 58.44147
 #' print(rpt$P)  #  0.0002706851
+#' @export
 dot_fisher <- function(Z, C, ...)
 {
     L <- length(Z)                      # number of statistics
-    within(dot(Z, C, ...),
-    {
-        ## de-correlated two-tail p-values, sorted
-        P <- sort(1 - pchisq(X^2, df=1))
-
-        Y <- -2 * sum(log(P))
-
-        P <- 1 - pchisq(Y, 2 * L)
-    })
+    ret <- dot(Z, C, ...)
+    
+    ## decorrelated two-tail p-values, sorted
+    P <- sort(1 - stats::pchisq(ret$X^2, df=1))
+    Y <- -2 * sum(log(P))
+    P <- 1 - stats::pchisq(Y, 2 * L)
+    c(list(P=P, Y=Y), ret)
 }
